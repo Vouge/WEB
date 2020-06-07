b93c5b0d2fd6ef3563987ac2b2fc2fdea1dccd4a
bipibohara <bipinbohara90@gmail.com>
2020-06-07 14:55:36 +0545

    Added route application.py file

---
+++ Flask/routes/application.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, world!"
+
+@app.route("/jack")
+def david():
+    return "Hello, Jack!"
+
+
+@app.route("/<string:name>")
+def hello(name):
+    name1 = ""
+    names = name.split(" ")
+    for name in names:
+        name1 += name.capitalize() + " "
+    return f"Hello, {name1}!"
